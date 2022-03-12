9de6c3ff357918dfd4f751b548d978ac2d2f32cd
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-03-12 07:05:08 +0000

    2-player functionality text entry working

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,8 +1,33 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from models.find_winner import *
 from models.player import Player
 
-@app.route('/Home')
+@app.route('/')
 def index():
-    return render_template('index.html', title='Home', players=players)
+    return render_template('index.html', title='Home')
+
+@app.route('/game')
+def show_game():
+    return render_template('game.html')
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html', title="Welcome")
+
+@app.route('/two_player')
+def enter_details():
+    return render_template('two_player.html', title="play" )
+
+@app.route('/game', methods = ['POST'])
+def play_game():
+    player1_name = request.form["p1name"]
+    player1_choice = request.form["p1choice"]
+    player2_name = request.form["p2name"]
+    player2_choice = request.form["p2choice"]
+    player1 = Player(player1_name, player1_choice)
+    player2 = Player(player2_name, player2_choice)
+    print(player1.name)
+    print(player2.name)
+    winner = play(player1, player2)
+    return render_template("game.html", title='winner', winner=winner, player1=player1, player2=player2)
--- models/find_winner.py
+++ models/find_winner.py
@@ -1,26 +1,28 @@
 from models.player import Player
 
-player1 = Player("Bill", "paper")
-player2 = Player("Tom", "rock")
+# player1 = Player("Bill", "rock")
+# player2 = Player("Tom", "paper")
 
-players = [player1, player2]
+# players = [player1, player2]
 
 def play(player1, player2):
+    if player1.choice == player2.choice:
+        return None
     if player1.choice == "scissors": 
         if player2.choice == "paper":
-            return f"player 1 wins with {player1.choice}"
+            return player1
         else:
-            return f"player 2 wins with {player2.choice}"
+            return player2
     if player1.choice == "paper": 
         if player2.choice == "rock":
-            return f"player 1 wins with {player1.choice}"
+            return player1
         else:
-            return f"player 2 wins with {player2.choice}"
+            return player2
     if player1.choice == "rock": 
         if player2.choice == "scissors":
-            return f"player 1 wins with {player1.choice}"
+            return player1
         else:
-            return f"player 2 wins with {player2.choice}"
+            return player2
 
     
     
